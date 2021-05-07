71f290e74838d1b3b7b3f476438135ae5468a973
PauloDev <devpgm2020@gmail.com>
2021-05-07 16:12:18 -0300

    Usando o axios para buscar dados dos gráficos da dashboard - forma errada

--- frontend/src/components/BarChart/DonutChart/index.tsx
+++ frontend/src/components/BarChart/DonutChart/index.tsx
@@ -1,25 +1,49 @@
+import axios from 'axios';
+
 import Chart from 'react-apexcharts';
+import { SaleSum } from 'types/sale';
+import { BASE_URL } from 'utils/requests';
+
+type ChartData = {
+  series: number[];
+  labels: string[];
+};
 
 const DonutChart = () => {
-	const mockData = {
-    series: [477138, 499928, 444867, 220426, 473088],
-    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-}
+	let chartData: ChartData = {labels: [], series:[]};
 
-const options = {
+	axios.get(`${BASE_URL}/sales/amount-by-seller`)
+	.then(response => {
+		const data = response.data as SaleSum[];
+		const labels = data.map(l => l.sellerName)
+		const series = data.map(s => s.sum)
+
+		chartData = {labels, series}
+
+		console.log(chartData)
+	})
+	.catch(error => console.error(error))
+
+
+  // const mockData = {
+  //   series: [477138, 499928, 444867, 220426, 473088],
+  //   labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé'],
+  // };
+
+  const options = {
     legend: {
-        show: true
-    }
-}
+      show: true,
+    },
+  };
 
-	return (
-		<Chart
-		options={{ ...options, labels: mockData.labels }}
-		series={mockData.series}
-		type="donut"
-		height="240"
-	/>
-	)
-}
+  return (
+    <Chart
+      options={{ ...options, labels: chartData.labels }}
+      series={chartData.series}
+      type="donut"
+      height="240"
+    />
+  );
+};
 
-export default DonutChart
+export default DonutChart;
